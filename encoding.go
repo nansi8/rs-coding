1d5b9bad90f88c926d1f266fd86178c3f7b19ba5
arturjoshi <rG#d*kz%3Hf1P>
2018-09-09 17:28:19 +0300

    [NSI] - moved block to the separate file

--- encoding.go
+++ encoding.go
@@ -5,20 +5,6 @@ import (
 	gomath "math"
 )
 
-type BlockType byte
-type BlockId int64
-
-const (
-	Data BlockType = iota
-	Checksum
-)
-
-type Block struct {
-	index     int
-	w         []byte
-	blockType BlockType
-}
-
 type Encoder struct {
 	blocks         []Block
 	dataBlocks     int
@@ -66,52 +52,4 @@ func (e *Encoder) Encode(input []byte) []Block {
 		blocks[i] = *block
 	}
 	return blocks
-}
-
-func getDataBlockMatrix(data []byte) [][]byte {
-	result := make([][]byte, len(data))
-	for i := 0; i < len(data); i++ {
-		result[i] = make([]byte, 1)
-		result[i][0] = data[i]
-	}
-	return result
-}
-
-func getCheckBlock(data [][]byte) []byte {
-	result := make([]byte, len(data))
-	for i := 0; i < len(data); i++ {
-		result[i] = data[i][0]
-	}
-	return result
-}
-
-func extend(data []byte, length int) []byte {
-	if length < len(data) {
-		return data
-	}
-	result := make([]byte, length)
-	copy(result, data)
-	return result
-}
-
-func vandermore(rows, cols int, alg math.ByteAlgebra) [][]byte {
-	result := make([][]byte, rows)
-	for i := 0; i < rows; i++ {
-		result[i] = make([]byte, cols)
-		for j := 0; j < cols; j++ {
-			result[i][j] = pow(byte(j+1), byte(i), alg)
-		}
-	}
-	return result
-}
-
-// returns a^x
-func pow(a, x byte, alg math.ByteAlgebra) byte {
-	if x == 0 {
-		return 1
-	}
-	if x == 1 {
-		return a
-	}
-	return alg.Mul(a, pow(a, x-1, alg))
 }
